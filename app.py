20236e8dc7172675d6ae9f41038c97819ce4bd37
arshia00j <arshia00iq@proton.me>
2024-09-22 19:05:05 +0330

    save

--- app.py
+++ app.py
@@ -33,3 +33,20 @@ def add_task(task: Task):
     tasks.append(task.dict())
 
     return {"message": "Task added successfully", "task": task}
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task: Task):
+
+    if task_id >= len(tasks):
+        return {"error": "Task not found"}
+    tasks[task_id-1] = task.dict()
+    return {"message": "Task updated successfully", "task": task}
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    if task_id >= len(tasks):
+        return {"error": "Task not found"}
+    removed_task = tasks.pop(task_id-1)
+    return {"message": "Task deleted successfully", "task": removed_task}
