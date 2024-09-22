42ce6c9d2aad0535487b2ce421097485facbde86
arshia00j <arshia00iq@proton.me>
2024-09-22 18:36:17 +0330

    add tasks and show_tasks()

--- app.py
+++ app.py
@@ -6,22 +6,23 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+class Task(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+tasks = [
+    {"title": "school homework",
+        "description": "solve 5 questions for math", "completed": False},
+    {"title": "fastapi", "description": "mini api with fastapi", "completed": True},
+    {"title": "art", "description": "draw one picture", "completed": False}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+]
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/tasks")
+def show_tasks():
+    return tasks
